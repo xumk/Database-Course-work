5cda89a32eda3cd4a653b53efedb4e3b751ec151
xumk <xumuk123@mail.ru>
2016-03-05 13:56:29 +0300

    Первый класс-сервлет а так же в Main добавлен логер

---
+++ CurseWork/src/main/java/Frontend.java
@@ -0,0 +1,22 @@
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * Created by Алексей on 05.03.2016.
+ */
+public class Frontend extends HttpServlet {
+    private String login = "";
+
+    @Override
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
+    @Override
+    public void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+}
--- CurseWork/src/main/java/Main.java
+++ CurseWork/src/main/java/Main.java
@@ -1,11 +1,15 @@
 import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.servlet.ServletContextHandler;
 import org.eclipse.jetty.servlet.ServletHolder;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * Created by Алексей on 05.03.2016.
  */
 public class Main {
+    private static Logger logger = LoggerFactory.getLogger(Main.class);
+
     public static void main(String[] args) {
         Frontend frontend = new Frontend();
         Server server = new Server(8080);
@@ -13,7 +17,11 @@ public class Main {
         server.setHandler(context);
         context.addServlet(new ServletHolder(frontend), "/authform");
 
-        server.start();
-        server.join();
+        try {
+            server.start();
+            server.join();
+        } catch (Exception e) {
+            logger.error("Ошибка запуска сервера", e);
+        }
     }
 }
